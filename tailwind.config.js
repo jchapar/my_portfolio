d1c6a22932b295eeab49df45d9cde9af996aef26
josechaparro <josechaparro.dev@gmail.com>
2022-09-19 23:33:44 -0700

    sidebar, landing section and about sections marks up completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fadedPurple: '#674DAA',
+        lightPurple: '#5D42A6',
+        mediumPurple: '#493382',
+        darkPurple: '#312450',
+        lightblue: '#4F52B5',
+        mediumBlue: '#4B4FAC',
+        darkBlue: '#474AA5',
+        darkPink: '#B74E91',
+        lightPink: '#BC5896',
+        darkGrey: '#766D88',
+        lightGrey: '#AFA2D2',
+        linkBorder: '#3C2C62',
+      },
+      fontFamily: {
+        sans: ['Source Sans Pro', 'sans-serif'],
+      },
+      backgroundImage: {
+        'header-bg': "url('/images/header-bg.jpg')",
+      },
+    },
   },
   plugins: [],
 };
